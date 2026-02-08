4bed2df069d9058b88e55add5e7a603cc20ee38d
John Villablanca <kodinghoodie@gmail.com>
2026-02-07 17:06:15 -0800

    Renamed files with numbers for better organization

---
+++ scrimba/04-string-slicing.py
@@ -0,0 +1,17 @@
+# Scrimba Challenge: Exercise - Strings - Basics/Slicing.
+# 1. From the string "Welcome to Python 101: Strings", extract text and create/print new string that says "1 Welcome Ring To Tyler"
+# Every first letter in a word should be capitalized(title format)
+# Print the same string backwards
+
+
+
+# My Solution to the challenge 
+
+msg='welcome to Python 101: Strings'
+  
+msg = 'welcome to Python 101: Strings'
+
+new_msg = msg[18] + " " + msg[0:7] + " " + msg[25:29] + " " + msg[8:10] + " " + (msg[8] + msg[12] + msg[2] + msg[6] + msg[25])
+
+print(new_msg.title())
+print(msg[::-1])
